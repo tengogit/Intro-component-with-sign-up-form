8b5ad385d69ef76aae6bb92ac9059ac9cf1c11c1
TENGO <tengogithub@gmail.com>
2026-02-07 14:46:29 +0400

    check

--- main.js
+++ main.js
@@ -3,40 +3,34 @@ const inputs = document.getElementsByClassName('input')
 const erroricon = document.querySelectorAll('.erroricon')
 const errorp = document.querySelectorAll('p')
 const email = document.getElementById('email')
-const check = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const erroremail = document.getElementById('emailerror')
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 btn.addEventListener('click', () => {
     let added = true;
+
     Array.from(inputs).forEach((input, i) => {
         if (input.value.trim() === "") {
             erroricon[i].classList.add('active')
             errorp[i].classList.add('active')
             added = false
-        }
-        else {
+        } else {
             erroricon[i].classList.remove('active')
-            errorp[i].classList.remove('active');
-
+            errorp[i].classList.remove('active')
         }
-
-
     })
     if (added) {
-        {
-            alert('SUCCESS')
-        }
-
+        alert('SUCCESS')
     }
-
-    Array.from(inputs).forEach((input, i) => {
-        input.addEventListener('input', () => {
-            if (input.value.trim() !== "") {
-                erroricon[i].classList.remove('active')
-                errorp[i].classList.remove('active')
-            }
-
-        })
-
-    });
-
 })
+
+Array.from(inputs).forEach((input, i) => {
+    input.addEventListener('input', () => {
+        if (input.value.trim() !== "") {
+            erroricon[i].classList.remove('active')
+            errorp[i].classList.remove('active')
+        }
+    })
+})
+
